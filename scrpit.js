5afe4b0a78c4a7ffa768103ef684ed97bd2c3c7a
Vitor Souza e Souza <87282092+VitorSouzaESouza@users.noreply.github.com>
2025-01-30 23:05:09 -0300

    Add files via upload

--- scrpit.js
+++ scrpit.js
@@ -12,25 +12,42 @@ $(function(){
             var height = parseFloat($('#input_height').val());
             var ans = weight/(height*height)
             ans = ans.toFixed(1)
+            var ideal_weight_min = (height*height)*18.5
+            ideal_weight_min = ideal_weight_min.toFixed(2)
+            var ideal_weight_max = (height*height)*24.9
+            ideal_weight_max = ideal_weight_max.toFixed(2)
 
-            var result = 'Seu IMC é ' + String(ans)
+            var imc = 'Seu IMC é ' + String(ans)
+            var ideal_weigth_interval = String(ideal_weight_min) + " Kg <= Seu peso ideal >= " + String(ideal_weight_max) + " Kg"
             if(ans<18.5){
-                result += ', abaixo do peso'
+                result = 'Está abaixo do peso'
                 $('#div_result').css('background-color', 'rgb(150,0,0)')
             }
-            else if(ans>=18.5 & ans<24.9){
-                result += ', peso normal'
-                $('#div_result').css('background-color', 'rgb(0,150,0)')
+            else if(ans>=18.5 & ans<=24.9){
+                if(ans<18.5*1.05){
+                    result = 'Está no peso ideal, mas no limite inferior'
+                    $('#div_result').css('background-color', 'rgb(100,150,0)')
+                }
+                else if(ans>24.9*0.95){
+                    result = 'Está no peso ideal, mas no limite superior'
+                    $('#div_result').css('background-color', 'rgb(100,150,0)')
+                }
+                else{
+                    result = 'Está no peso ideal'
+                    $('#div_result').css('background-color', 'rgb(0,100,0)')
+                }
             }
-            else if(ans>=25 & ans<29.9){
-                result += ', acima do peso'
+            else if(ans>=25 & ans<=29.9){
+                result = 'Está acima do peso'
                 $('#div_result').css('background-color', 'rgb(150,0,0)')
             }
             else{
-                result += ', obeso'
-                $('#div_result').css('background-color', 'rgb(150,0,0)')
+                result = 'Está obeso'
+                $('#div_result').css('background-color', 'rgb(255,0,0)')
             }
+            $('#div_imc').text(imc)
             $('#div_result').text(result)
+            $('#div_ideal_weight').text(ideal_weigth_interval)
         }
         else{
             if($('#input_weight').val() <= 0){
